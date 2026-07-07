f04ff15ded52132019fa0a7ec1fbcf2f40bfae12
joshuawyrick <jwyrick7@gmail.com>
2026-07-07 08:24:21 -0700

    Updated index.ts

--- supabase/functions/create-checkout/index.ts
+++ supabase/functions/create-checkout/index.ts
@@ -63,7 +63,6 @@ Deno.serve(async (req: Request) => {
         quantity: 1,
       }],
       mode: "payment",
-      allow_promotion_codes: true,
       success_url: `${origin}/?checkout=success&session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/?checkout=cancel`,
       metadata: {
